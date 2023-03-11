83623358fc08555e4e97d5c9bf41d950277226e5
Eduard <edwardbayron@gmail.com>
2023-03-11 21:11:19 +0200

    refined MainActivity for prepare a list

--- app/src/main/java/com/loanapp/app/MainActivity.kt
+++ app/src/main/java/com/loanapp/app/MainActivity.kt
@@ -3,13 +3,19 @@ package com.loanapp.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.loanapp.app.ui.theme.LoanApp_v3Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HomeContainer("Android")
                 }
             }
         }
@@ -30,14 +36,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeContainer(name: String) {
+    Column {
+        AppBarView()
+        HomeDataContainer()
+
+    }
+}
+
+@Composable
+fun AppBarView(){
+    TopAppBar(
+        elevation = 4.dp,
+        title = {
+            Text("I'm a TopAppBar")
+        },
+        backgroundColor =  MaterialTheme.colors.primarySurface,
+        navigationIcon = {
+            IconButton(onClick = {/* Do Something*/ }) {
+                Icon(Icons.Filled.ArrowBack, null)
+            }
+        }, actions = {
+            IconButton(onClick = {/* Do Something*/ }) {
+                Icon(Icons.Filled.Share, null)
+            }
+            IconButton(onClick = {/* Do Something*/ }) {
+                Icon(Icons.Filled.Settings, null)
+            }
+        })
+}
+
+@Composable
+fun HomeDataContainer(){
+    Card(modifier = Modifier.fillMaxSize()) {
+
+        Text("Hello World") //TODO should be a main list of views
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LoanApp_v3Theme {
-        Greeting("Android")
+        HomeContainer("Android")
     }
 }
